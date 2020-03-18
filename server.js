e39bee51d9664804e32768e2220814d402908154
dna-dev <dna-dev@pop-os.localdomain>
2020-03-19 07:25:00 +0800

    Add new logic in 'loginRoute' to get all employees by role

--- server.js
+++ server.js
@@ -17,14 +17,58 @@ const internal_error = 'Internal Server Error';
 const roles = ['ceo', 'assistant', 'president', 'hr', 'pm', 'senior developer', 'junior developer'];
 
 const loginRoute = async (req, res) => {
-  let conn;
+  let conn, roles_to_select, employees;
   try {
 
     /* check if login credentials are provided */
     if (!req.body.user_name || !req.body.password) {
       return res.send(400, { message: invalid_data });
     }
+    /* initialize connection here */
+    conn = await r.connect();
+    const [user] = await r.db('test').table('company_users')
+      .getAll(req.body.user_name, { index: 'user_name' })
+      .filter(user => user('password').eq(req.body.password))
+      .coerceTo('array')
+      .run(conn);
 
+    if (!user) {
+      return res.send(400, { message: 'user does not exists' });
+    }
+
+
+    /* get role for the specific user */
+    const { role } = user;
+
+    switch (role) {
+      case 'ceo':
+        roles_to_select = roles.slice(1);
+        break;
+      case 'president':
+        roles_to_select = roles.slice(3);
+        break;
+      case 'hr':
+        roles_to_select = roles.slice(4);
+        break;
+      case 'pm':
+        roles_to_select = roles.slice(5);
+        break;
+      case 'senior developer':
+        roles_to_select = roles.slice(6);
+        break;
+      default:
+        roles_to_select = undefined;
+        break;
+    }
+
+    if (roles_to_select) {
+      employees = await r.db('test').table('company_users')
+        .getAll(...roles_to_select, { index: 'role' })
+        .coerceTo('array')
+        .run(conn);
+    }
+
+    res.send(200, { user, employees });
   } catch (e) {
     res.send(500, { message: internal_error });
   } finally {
