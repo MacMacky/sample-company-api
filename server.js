f4a58c9f7d1f6703828f63c657f33fd7f7836f9d
dna-dev <dna-dev@pop-os.localdomain>
2020-03-19 10:32:09 +0800

    Modify functionality for 'loginRoute' and fix errors in 'createUserRoute'

--- server.js
+++ server.js
@@ -5,19 +5,22 @@ const r = require('rethinkdb');
 const port = 3000;
 const server = restify.createServer();
 
+/* Helpers */
+const isString = val => typeof val === 'string';
+/* Helpers End */
 
-/* RESPONSES */
+/* Response */
 const invalid_data = 'invalid data provided.';
 const invalid_role = 'role not valid!';
 const invalid_id = 'Invalid id!';
 const id_does_not_exists = 'id does not exists.';
 const internal_error = 'Internal Server Error';
-/* RESPONSES */
+/* Responses End */
 
 const roles = ['ceo', 'assistant', 'president', 'hr', 'pm', 'senior developer', 'junior developer'];
 
 const loginRoute = async (req, res) => {
-  let conn, roles_to_select, employees;
+  let conn, roles_to_select, employees, user;
   try {
 
     /* check if login credentials are provided */
@@ -26,9 +29,8 @@ const loginRoute = async (req, res) => {
     }
     /* initialize connection here */
     conn = await r.connect();
-    const [user] = await r.db('test').table('company_users')
+    [user] = await r.db('test').table('employees')
       .getAll(req.body.user_name, { index: 'user_name' })
-      .filter(user => user('password').eq(req.body.password))
       .coerceTo('array')
       .run(conn);
 
@@ -36,6 +38,11 @@ const loginRoute = async (req, res) => {
       return res.send(400, { message: 'user does not exists' });
     }
 
+    const { password } = user;
+    /* check if password is correct */
+    if (password !== req.body.password) {
+      return res.send(400, { message: 'wrong password. please try again.' });
+    }
 
     /* get role for the specific user */
     const { role } = user;
@@ -62,7 +69,7 @@ const loginRoute = async (req, res) => {
     }
 
     if (roles_to_select) {
-      employees = await r.db('test').table('company_users')
+      employees = await r.db('test').table('employees')
         .getAll(...roles_to_select, { index: 'role' })
         .coerceTo('array')
         .run(conn);
@@ -90,10 +97,72 @@ const getUsersRoute = async (req, res) => {
 
 
 const createUserRoute = async (req, res) => {
-  let conn;
+  let conn, user;
   try {
 
+    /* check if user_name is provided */
+    if (!req.body.user_name || !isString(req.body.user_name)) {
+      return res.send(400, { message: invalid_data });
+    }
+
+    /* check if password is provided */
+    if (!req.body.password || !isString(req.body.password)) {
+      return res.send(400, { message: invalid_data });
+    }
+
+    /* check if full name is provided */
+    if (!req.body.full_name || !isString(req.body.password)) {
+      return res.send(400, { message: invalid_data });
+    }
+
+    /* check if role is provided */
+    if (!req.body.role || !isString(req.body.role)) {
+      return res.send(400, { message: invalid_data });
+    }
+
+    /* check if role is valid */
+    if (!roles.includes(req.body.role.toLowerCase())) {
+      return res.send(400, { message: invalid_role });
+    }
+
+    /* initialize connection here */
+    conn = await r.connect();
+
+    [user] = await r.db('test').table('employees')
+      .getAll(req.body.user_name, { index: 'user_name' })
+      .coerceTo('array')
+      .run(conn);
+
+    /* check if user_name is taken */
+    if (user) {
+      return res.send(400, { message: 'user_name is already taken. please choose another one.' });
+    }
+
+
+    /* check if role of ceo and president is already taken */
+    if (req.body.role.toLowerCase() === 'president' || req.body.role.toLowerCase() === 'ceo') {
+
+      [user] = await r.db('test').table('employees')
+        .getAll(req.body.role.toLowerCase(), { index: 'role' })
+        .coerceTo('array')
+        .run(conn);
+
+      if (user && (user.role === 'president' || user.role === 'ceo')) {
+        return res.send(400, { message: `role for ${user.role} is already taken. please try another one.` });
+      }
+    }
+
+    const { first_error, generated_keys } = await r.db('test').table('employees')
+      .insert({ ...req.body, role: req.body.role.toLowerCase() })
+      .run(conn);
+
+
+    return res.send(first_error ? 400 : 200,
+      first_error ? { message: 'Cant insert data. Please try again later.' }
+        : { ...req.body, role: req.body.role.toLowerCase(), id: generated_keys[0] });
+
   } catch (e) {
+    console.log(e);
     res.send(500, { message: internal_error });
   } finally {
     conn && conn.close();
@@ -113,28 +182,39 @@ const removeUserRoute = async (req, res) => {
 }
 
 
+const updateUserRoute = async (req, res) => {
+  let conn;
+  try {
+
+  } catch (e) {
+    res.send(500, { message: internal_error });
+  } finally {
+    conn && conn.close();
+  }
+}
+
 
 server.use(restify.plugins.bodyParser());
 server.post('/api/login', loginRoute);
-server.get('/api/users/:role', getUsersRoute);
-server.post('/api/users', createUserRoute);
-server.del('/api/users/:id/role/:role', removeUserRoute);
+server.get('/api/employees/:id', getUsersRoute);
+server.post('/api/employees', createUserRoute);
+server.put('/api/employees/:id', updateUserRoute);
+server.del('/api/employees/:id/role/:role', removeUserRoute);
 server.get('*', (req, res) => res.send(404));
 server.post('*', (req, res) => res.send(404));
 server.put('*', (req, res) => res.send(404));
 server.del('*', (req, res) => res.send(404));
 
 
-const indexCreate = async (con, index_name, table_name = 'company_users') => {
+const indexCreate = async (con, index_name, table_name = 'employees') => {
   return r.db('test').table(table_name).indexCreate(index_name).run(con);
 };
 
-server.listen(port, () => {
+server.listen(port, async () => {
   let conn;
   try {
     conn = await r.connect();
-    const indexes_made = await r.db('test').table('company_users').indexList().run(conn);
-
+    const indexes_made = await r.db('test').table('employees').indexList().run(conn);
     /* create indexes if they don't exist already */
     ['role', 'user_name'].filter(item => !indexes_made.includes(item))
       .forEach(index_name => indexCreate(conn, index_name)
